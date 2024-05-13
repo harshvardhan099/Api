17a507b0ec7c105e46a569d2c9880fe5f7f7cf7b
harshvardhan099 <harshvardhan.singh@myccmg.com>
2024-05-13 10:41:35 +0530

    Python file api_1.py modified

--- api_1.py
+++ api_1.py
@@ -4,10 +4,14 @@ from typing import List, Dict
 
 app = FastAPI()
 
+organization = [
+    "Colllance", "CCMG"
+]
+
 
 @app.get("/getOrganizations")
 def get_companies() -> Dict[str, List[str]]:
-    return {"data": ["Collance", "CCMG"]}
+    return {'data': organization}
 
 
 if __name__ == "__main__":
